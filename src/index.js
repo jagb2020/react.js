fd672b235a12e169fce291a639437eea9bda3426
Angel Gonzalez <agb92@live.com>
2021-02-22 23:46:41 -0600

    JSX

--- src/index.js
+++ src/index.js
@@ -1,8 +1,21 @@
 import React from "react";
 import ReactDOM from "react-dom";
 
-const element = <h1>Hello, React JS!</h1>;
+// const jsx = (
+//   <div>
+//     <h1>Hola, soy Angel</h1>
+//     <p>Soy ingeniero frontend.</p>
+//   </div>
+// );
+
+const element = React.createElement(
+  "div",
+  {},
+  React.createElement("h1", {}, "Hola, soy Angel"),
+  React.createElement("p", {}, "Soy ingeniero de la web.")
+);
 
 const container = document.getElementById("app");
 
+// ReactDOM.render(jsx, container);
 ReactDOM.render(element, container);
